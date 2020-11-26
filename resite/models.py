782806ea210ba8c17f16ca194fb83e6bf6fda30d
Vrishab Srivatsa <vsrivatsa25@gmail.com>
2020-11-26 15:58:04 +0530

    Fixed

--- resite/models.py
+++ resite/models.py
@@ -8,4 +8,7 @@ class Contact(models.Model):
     message = models.CharField(max_length=1000)
     time = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name +", "+ self.time
+
 admin.site.register(Contact)
