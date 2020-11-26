3b16c2bf50196225d6988adec4782d7d5014657a
Vrishab Srivatsa <vsrivatsa25@gmail.com>
2020-11-26 16:03:17 +0530

    Fixed

--- myresume/settings.py
+++ myresume/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@im#decne)hm8om(i%h7!=#ih=ql(%kwgiocjtcvs$&=$y(foj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
