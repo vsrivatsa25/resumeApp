e2e413a9bf66007a7d556805bf1279be485bd0a3
Vrishab Srivatsa <vsrivatsa25@gmail.com>
2021-12-19 00:11:07 +0530

    Minor changes

--- resite/static/js/splash_screen.js
+++ resite/static/js/splash_screen.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
+    $("html, body").animate({ scrollTop: 0 }, "slow");
     $('.loadingbar').delay(500).animate({left: '0'}, 2000);
     $('.loadingBox').delay(0).animate({opacity: '1'}, 500);
     $('.splashScreen').delay(2500).animate({top: '-100%'}, 1500);
-    $('.loadingCircle').delay(2500).animate({opacity: '0'}, 1500);
     $('.splashBehind').delay(4000).animate({opacity: '1'}, 1000);
 	$('body').addClass("visibleSplash");
 });
