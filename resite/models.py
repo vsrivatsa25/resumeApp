bfaa2376dae24385e145b24ebdf7b2cb99669e37
Vrishab Srivatsa <vsrivatsa25@gmail.com>
2022-07-31 22:37:12 +0530

    Migrations in db

--- resite/models.py
+++ resite/models.py
@@ -5,7 +5,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=20)
     email = models.CharField(max_length=50)
     subject = models.CharField(max_length=50)
-    message = models.CharField(max_length=1000)
+    message = models.TextField(max_length=1000)
     time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
